22f7e9f6cf10401f72777f6386a19cf73d4f5ce8
jackWilliamFreeman <wfreeman@fmgl.com.au>
2022-11-22 12:09:26 +0800

    added env variables etc

--- rds_source/learning_db.py
+++ rds_source/learning_db.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
+import os
 
 class learning_db():
+
+    name = os.getenv("NAME")
+    password = os.getenv("PASSWORD")
+    rds_host = os.getenv("RDS_HOST")
+    db_name = os.getenv("DB_NAME")
 
     def get_connection(engine):
         connection = engine.connect()
